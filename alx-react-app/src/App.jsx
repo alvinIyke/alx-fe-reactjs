e2ecd54a40beb97f473cb5979f4390184885ebd7
alvinIyke <alvin.uchey@yahoo.com>
2024-11-02 13:49:37 +0100

    Specific React Components created.

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,8 +1,11 @@
 import WelcomeMessage from './WelcomeMessage'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -30,6 +33,9 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </>
   )
 }
