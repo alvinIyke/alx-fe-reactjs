b2794a1d27f1f08af0e778b7468a24c41ba6b48c
alvinIyke <alvin.uchey@yahoo.com>
2024-11-07 14:36:18 +0100

    Added styling to React App

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent(){
+    return (
+        <>
+         <main>
+         <p style = {{color: 'orange', fontStyle:'italic'}}>I love to visit New York, Paris, and Tokyo.</p>
+         </main>
+        </>
+    );
+}
+export default MainContent;
+
