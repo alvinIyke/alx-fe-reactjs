4464a66fffd1862b1e74aa86b31f4c320d498c34
alvinIyke <alvin.uchey@yahoo.com>
2024-11-08 10:23:22 +0100

    updated app.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -4,9 +4,9 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
-function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
-
+  
+  function App() {
   return (
     <UserContext.Provider {...userData}>
      <ProfilePage />
