ab73e67a8c63446320e3dda45187e199461197c1
alvinIyke <alvin.uchey@yahoo.com>
2024-11-16 14:42:14 +0100

    app.jsx updated

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,16 +1,31 @@
-import { useState } from 'react';
-import  RecipeList  from './components/RecipeList';
-import  AddRecipeForm  from './components/AddRecipeForm';
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import RecipeList from './components/RecipeList'; // Correct import
+import AddRecipeForm from './components/AddRecipeForm'; // Correct import
+import RecipeDetails from './components/RecipeDetails';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function   App() {
   return (
-    <>
-      Hello World
-    </>
-  )
+    <Router>
+      <div>
+        <h1>Recipe Sharing App</h1>
+        <AddRecipeForm />
+        <RecipeList />
+      </div>
+
+      {/* Define Routes */}
+      <Routes>
+        <Route path="/" element={<RecipeList />} />
+        <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+// Wrapper to extract recipeId from route params
+const RecipeDetailsWrapper = () => {
+  const { id } = useParams();
+  return <RecipeDetails recipeId={parseInt(id, 10)} />;
+};
+
+export default App;
