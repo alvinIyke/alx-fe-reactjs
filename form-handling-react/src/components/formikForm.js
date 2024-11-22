9ba5e6549eda136041216f92daa91c4ef29b16ab
alvinIyke <alvin.uchey@yahoo.com>
2024-11-22 18:33:19 +0100

    Yup Validation schema

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -10,18 +10,38 @@ const RegistrationForm = () => {
   
 // Validation Schema
 const validationSchema = Yup.object({
-    username: Yup.string()
-       .required('Username is required'),
-    email: Yup.string()
-       .email('Invalid email address')
-       .required('Email is required')
-    password: Yup.string()
-       .min(8,'Password must be at least 8 characters')
-       .required('Password is required')
+    username: Yup.string().required('Username is required'),
+    email: Yup.string().email('Invalid email address').required('Email is required')
+    password: Yup.string().min(8,'Password must be at least 8 characters').required('Password is required')
     
 });
 
-const FormikForm = () => (
+const onSubmit = async (values, { setSubmitting, resetForm }) => {
+    try {
+           // Simulate API call
+        await new Promise(resolve => setTimeout(resolve, 400))
+
+          // Extract values for submission
+        const { username, email, password } = values;
+
+          // Perform registration logic
+        console.log('Registration Data:', { username, email });
+
+          // Submit the form data to the server or API
+        console.log('Form submitted:', values);
+           
+          // Reset form and stop submission
+        resetForm();
+        alert('Registration Successful!');
+    } catch (error) {
+        console.error('Registration failed', error);
+    } finally {
+        setSubmitting(false);
+    }
+  };
+
+
+ return (
     <Formik
         initialValues={{ username: '', email: '', password: ''}}
         validationSchema={validationSchema}
@@ -44,4 +64,4 @@ const FormikForm = () => (
 )};
 
 
-export default FormikForm;
+export default RegistrationForm;
