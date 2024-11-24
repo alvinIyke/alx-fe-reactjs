f534531782e83fecbeea9a3f6ce665688f0d03c6
alvinIyke <alvin.uchey@yahoo.com>
2024-11-24 13:10:29 +0100

    Implementation of testing component, TodoList component and TodoList.test.js component

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -1,3 +1,64 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import React from 'react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import TodoList from '../components/TodoList';
 import TodoForm from '../components/AddTodoForm';
+
+
+describe('TodoList component', () => {
+  it('renders the todo list', () => {            // Initial rendering of the TodoList component
+    const { getByText } = render(<TodoList />);
+    expect(getByText('Buy milk')).toBeInTheDocument();
+    expect(getByText('Walk the dog')).toBeInTheDocument();
+    expect(getByText('Do laundry')).toBeInTheDocument();
+  });
+
+ 
+  it('toggles the todo completed state', () => {
+    const { getByLabelText } = render(<TodoList />);
+    const checkbox = getByLabelText('Buy milk');
+    fireEvent.click(checkbox);
+    expect(checkbox).toBeChecked();
+  });
+
+  // Test for adding a todo item
+  it('adds a new todo item', () => {
+    const { getByPlaceholderText, getByText } = render(<TodoList />);
+    const input = getByPlaceholderText('Enter a new todo');
+    const addButton = getByText('Add Todo');
+
+    fireEvent.change(input, { target: { value: 'New Todo' } });
+    fireEvent.click(addButton);
+
+    expect(getByText('New Todo')).toBeInTheDocument();
+  });
+ 
+ // Test for toggling between completed and not completed
+  it('toggles between completed and not completed', () => {
+    const { getByLabelText, getByText } = render(<TodoList />);
+    const checkbox = getByLabelText('Buy milk');
+    const todoText = getByText('Buy milk');
+
+    fireEvent.click(checkbox);
+    expect(checkbox).toBeChecked();
+    expect(todoText).toHaveStyle('textDecoration: line-through');
+
+    fireEvent.click(checkbox);
+    expect(checkbox).not.toBeChecked();
+    expect(todoText).not.toHaveStyle('textDecoration: line-through');
+  });
+
+ // Test for deleting a todo item
+  it('deletes a todo item', () => {
+    const { getByText, queryByText } = render(<TodoList />);
+    const deleteButton = getByText('Delete');
+    fireEvent.click(deleteButton);
+    expect(queryByText('Buy milk')).not.toBeInTheDocument();
+  });
+
+  // Test to show that the correct number of todo items is rendered
+  it('renders the correct number of todo items', () => {
+    const { getAllByRole } = render(<TodoList />);
+    const todoItems = getAllByRole('listitem');
+    expect(todoItems).toHaveLength(3);
+  });
+});
