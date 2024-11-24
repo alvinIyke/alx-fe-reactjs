7c197bfea45c8f4bbd16c75c1582b6e767186b36
alvinIyke <alvin.uchey@yahoo.com>
2024-11-24 13:47:56 +0100

    copied TodoList.test.js file to the components folder

--- react-todo/src/components/TodoList.test.js
+++ react-todo/src/components/TodoList.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-import TodoList from '../components/TodoList';
-import TodoForm from '../components/AddTodoForm';
+import TodoList from './TodoList';
+import TodoForm from './AddTodoForm';
 
 
 describe('TodoList component', () => {
