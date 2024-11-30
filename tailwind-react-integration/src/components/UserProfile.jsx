995611d0ea6b0ba4040edd7a1ece077ea5a570ce
alvinIyke <alvin.uchey@yahoo.com>
2024-11-30 13:44:10 +0100

    Responsive image sizing corrected

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -6,7 +6,7 @@ import '../App.css';
 function UserProfile() {
     return (
       <div className="bg-gray-100 sm:p-4 md:p-8 md:max-w-sm sm:max-w-xs mx-auto my-20 rounded-lg shadow-lg">
-        <img className="rounded-full md:w-36h-36 sm: w-24h-24 mx-auto"  src="https://via.placeholder.com/150" alt="User" />
+        <img className="rounded-full md:w-36 md:h-36 sm:w-24 sm:h-24 mx-auto"  src="https://via.placeholder.com/150" alt="User" />
         <h1 className="sm:text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
         <p className="text-gray-600 sm:text-sm md:text-base"> Developer at Example Co. Loves to write code and explore new technologies.</p>
       </div>
