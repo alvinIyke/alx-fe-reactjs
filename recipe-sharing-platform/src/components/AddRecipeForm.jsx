a5a2bb0ab1e2a5adcced1e6d11429a5468d03a92
alvinIyke <alvin.uchey@yahoo.com>
2024-12-05 18:30:15 +0100

    added styles for medium screens

--- recipe-sharing-platform/src/components/AddRecipeForm.jsx
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -14,7 +14,7 @@
       });
     const [isFormValid, setIsFormValid] = useState(false);
 
-      const validateForm = () => {
+      const validateForm = () => {   
         const newErrors = {
           title: '',
           ingredients: '',
@@ -57,7 +57,7 @@
 
     return (
       
-      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:px-7 lg:px-8">
        <h1 className="text-3xl font-bold mb-4">Add a New Recipe Here</h1>
        <form onSubmit={handleSubmit}>
       
