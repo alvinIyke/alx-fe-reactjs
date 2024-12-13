ba0c7533e05d6eaca357ce42e8070fadc71dd375
alvinIyke <alvin.uchey@yahoo.com>
2024-12-13 12:58:37 +0100

    API integration done

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,27 +1,12 @@
-import axios from 'axios';
 
-const GITHUB_API_URL = '(https://api.github.com/users/{username})';
 
-const githubService = {
-  async getUser(username) {
-    try {
-      const response = await axios.get(`${GITHUB_API_URL}/users/${username}`);
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
-  },
-
-  async getRepos(username) {
-    try {
-      const response = await axios.get(`${GITHUB_API_URL}/users/${username}/repos`);
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
-  },
+const fetchUserData = async (searchTerm) => {
+  try {
+    const userData = await githubService.getUser(searchTerm);
+    return userData;
+  } catch (error) {
+    throw error;
+  }
 };
 
-export default githubService;
-
-
+export default fetchUserData;
