9a71260127354f189b9c9424acc477994b9df0bf
alvinIyke <alvin.uchey@yahoo.com>
2024-12-13 14:37:12 +0100

    styled search component using TailwindCSS and increased search criteria

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -41,12 +41,13 @@ function Search() {
       ) : error ? (
         <p style={{ color: 'red' }}>Looks like we cant find the user.</p>
       ) : userData ? (
-        <div>
+        <div className='bg-gray-600 text-white p-4 my-12 mx-2 sm:px-6 md:px-7 lg:px-8'>
           <h2>User Information</h2>
           <img src={userData.avatar_url} alt={userData.name} width="100" height="100" />
           <h3>
             <a href={userData.html_url} target="_blank" rel="noopener noreferrer">
-              {userData.name} ({userData.login})
+              {userData.name} ({userData.login}) ({userData.location}) ({userData.minimumRepositories})
+
             </a>
           </h3>
         </div>
